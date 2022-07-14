2ad04f9324104b9d295fa432e6a3b6fd61fbb519
Jen Cheng <nd5726@gmail.com>
2022-07-14 15:59:35 +0800

    [edit] path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  mode: 'jit'
-}
+  mode: "jit",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/fongji_web/" // 這裡要換成你的儲存庫名稱
+      : "/",
+};
